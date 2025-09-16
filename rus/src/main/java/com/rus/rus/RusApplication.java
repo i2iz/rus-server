62eeead97a8e2d9ab1d2b6508578d628c93f291d
김민준 <minjoon9560@gmail.com>
2025-09-16 15:37:27 +0900

    initial commit

---
+++ rus/src/main/java/com/rus/rus/RusApplication.java
@@ -0,0 +1,25 @@
+package com.rus.rus;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class RusApplication {
+
+	/**
+	 * 애플리케이션의 기본 시간대를 Asia/Seoul로 설정합니다.
+	 */
+	@PostConstruct
+	public void setDefaultTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusApplication.class, args);
+	}
+
+}
---
+++ rus/src/test/java/com/rus/rus/RusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rus.rus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
